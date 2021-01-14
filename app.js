ff53ef9024824bff97c4418a8fc205d0c6e25485
oabaiadze <oabaiadze@gmail.com>
2021-01-14 14:08:45 +0400

    Changed Port number changes added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Infrastructure as a code IssS!'));
+app.get('/', (req, res) => res.send('Infrastructure as a code IssS! hanges made By me its inmteresting')
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
