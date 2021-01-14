3fe9bf818d919919cb4f64b5be6f672b3980bbe3
oabaiadze <oabaiadze@gmail.com>
2021-01-14 13:11:38 +0400

    CI/CD First step from Linux geek :D

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8181;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Infrastructure as a code IssS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
